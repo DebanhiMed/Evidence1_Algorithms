71f48e3bf18454ba5e30359fab4a451e2ae5a6ac
debbelzz <A00835550@tec.mx>
2024-10-03 22:46:52 -0600

    main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main(){
+    string file1 = "transmission1.txt";
+    string file2 = "transmission2.txt";
+    string file3 = "transmission3.txt";
+    string malicious = "mcode.txt";
+    string outputFile = "checking.txt"; //debe generar este archivo
+
+    vector<string> T1;
+    vector<string> T2;
+    vector<string> T3;
+}
